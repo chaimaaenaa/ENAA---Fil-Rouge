abd1597c83ce4e45535538bff89de2775ca1809e
chaimaaenaa <mekayssichaimaaenaa@gmail.com>
2024-07-27 19:51:05 +0100

    add projet benifit

---
+++ BeniFit/src/main/java/com/benefit/benefit/BeniFitApplication.java
@@ -0,0 +1,13 @@
+package com.benefit.benefit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeniFitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeniFitApplication.class, args);
+	}
+
+}
---
+++ BeniFit/src/test/java/com/benefit/benefit/BeniFitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benefit.benefit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeniFitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
